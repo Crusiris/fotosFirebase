353b3fced5b053752201956a14c44da3300c687f
crusiris <cruzsirys@gmail.com>
2019-07-18 13:02:27 -0400

    modelo de datos

--- src/app/models/file-item.ts
+++ src/app/models/file-item.ts
@@ -8,4 +8,17 @@ export class FileItem {
     public url: string;
     public goingUp: boolean;
     public progress: number;
+
+
+
+ constructor( file: File){
+
+    this.file = file;
+    this.filename = file.name;
+
+
+    this.goingUp = false;
+    this.progress = 0;
+
+ }
 }
