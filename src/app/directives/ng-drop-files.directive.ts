87427d9a5eec01f4d3cc855ace16d6b0d0af076e
crusiris <cruzsirys@gmail.com>
2019-07-19 10:26:29 -0400

    validaciones

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[]= [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -20,5 +22,38 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit( false );
   }
 
+  //Validaciones
+   
+private _subir( archivo:File ): boolean {
 
+if (!this._archivoExiste(archivo.name) && this._esImagen( archivo.type ) ){
+  return true;
+}else {
+  return false;
+}
+
+}
+
+  private _prevenirDetener( event ){
+    event.preventDefaul();
+    event.stopPropagation();
+  }
+  
+
+  private _archivoExiste ( nameFile: string ): boolean {
+
+    for ( const archivo of this.archivos ){
+      if ( archivo.filename ===  nameFile ){
+        console.log('el archivo' + nameFile + 'ya existe');
+        return true;
+      }
+    }
+
+    return false;
+
+  }
+
+  private _esImagen( tipoArchivo:string ): boolean{
+    return  ( tipoArchivo === '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
+  }
 }
